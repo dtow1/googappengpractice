9357bb7f183ce2b81a48fb3c8e05c74e19c57ed3
Jason <jboy81@gmail.com>
2016-08-08 10:28:52 -0700

    refactored to retain form data on submit

--- firstapp/main.py
+++ firstapp/main.py
@@ -31,13 +31,13 @@ form = """
     What is your birthday?
     <br>
     <label> Month
-        <input type="text" name="month">
+        <input type="text" name="month" value="%(month)s">
     </label>
     <label> Day
-        <input type="text" name="day">
+        <input type="text" name="day" value="%(day)s">
     </label>
     <label> Year
-        <input type="text" name="year"
+        <input type="text" name="year" value="%(year)s">
     </label>
 
     <div style="color: red">%(error)s</div>
@@ -76,8 +76,11 @@ def valid_year(year):
     return  int(year) if (year.isdigit() and int(year) in range(1900,2021)) else None
 
 class MainHandler(webapp2.RequestHandler):
-    def write_form(self, error=""):
-        self.response.out.write(form % {"error": error})
+    def write_form(self, error="", month="", day="", year=""):
+        self.response.out.write(form % {"error": error,
+                                        "month": month,
+                                        "day": day,
+                                        "year": year})
 
     def get(self):
         # response represents the response that we will send back to the
@@ -85,12 +88,16 @@ class MainHandler(webapp2.RequestHandler):
         self.write_form()
 
     def post(self):
-        user_month = valid_month(self.request.get('month'));
-        user_day = valid_day(self.request.get('day'));
-        user_year = valid_year(self.request.get('year'));
+        user_month = self.request.get('month')
+        user_day = self.request.get('day')
+        user_year = self.request.get('year')
 
-        if not (user_month and user_day and user_year):
-            self.write_form("Wrong!")
+        month = valid_month(user_month)
+        day = valid_day(user_day)
+        year = valid_year(user_year)
+
+        if not (month and day and year):
+            self.write_form("Wrong!", user_month, user_day, user_year)
         else:
             self.response.out.write("Thanks! That is a valid day!")
 
